9011a52ca85f05379a08e6d0fb58d9972a502fac
itrikcode <kumarviveklohaman@gmail.com>
2023-12-28 08:17:07 +0530

    project initial commit

---
+++ src/main/java/com/itsp/sbintegrationwithfrontend/SbIntegrationWithFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.itsp.sbintegrationwithfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbIntegrationWithFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbIntegrationWithFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsp/sbintegrationwithfrontend/SbIntegrationWithFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsp.sbintegrationwithfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbIntegrationWithFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
